f4b8a70d1ac0c19b7c6909a60fb418535ff515c0
Guilherme Bordotti <gbordotti20@gmail.com>
2021-09-15 21:23:10 -0300

    teste

--- p0/first/urls.py
+++ p0/first/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import hw, pessoa
 
 urlpatterns = [
-    path('hello/', hw, name='hello'),
-    path('pessoa/', pessoa, name='list_pessoa')
+    path('hello', hw, name='hello'),
+    path('pessoa', pessoa, name='list_pessoa')
 ]
