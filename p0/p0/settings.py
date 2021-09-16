0fb9941bda8f3558c51c2ecac9f7fdb453ff8cf9
Guilherme Bordotti <gbordotti20@gmail.com>
2021-09-15 21:21:18 -0300

    Inserted allowed hosts

--- p0/p0/settings.py
+++ p0/p0/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u27@yui$26&ody4biwfvs(l48t)ufnl**hpgm)sgdlxxnrs0d7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 APPEND_SLASH = False
 
